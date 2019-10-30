58aab1c2bfcf2fc74b1df1085c8615fc44b8f13a
Joe Grotto <joegrotto@heir.company>
2019-10-30 18:59:30 -0500

    link component with GA outbound link anchor tag

---
+++ src/components/link.js
@@ -0,0 +1,87 @@
+import React from 'react'
+import { Link as GatsbyLink } from 'gatsby'
+import PropTypes from 'prop-types'
+
+// reused from https://github.com/gatsbyjs/gatsby/blob/master/packages/gatsby-plugin-google-analytics/src/index.js
+function OutboundLink(props) {
+  return (
+    <a
+      {...props}
+      onClick={e => {
+        if (typeof props.onClick === `function`) {
+          props.onClick(e)
+        }
+        let redirect = true
+        if (
+          e.button !== 0 ||
+          e.altKey ||
+          e.ctrlKey ||
+          e.metaKey ||
+          e.shiftKey ||
+          e.defaultPrevented
+        ) {
+          redirect = false
+        }
+        if (props.target && props.target.toLowerCase() !== `_self`) {
+          redirect = false
+        }
+        if (window.ga) {
+          window.ga(`send`, `event`, {
+            eventCategory: `Outbound Link`,
+            eventAction: `click`,
+            eventLabel: props.href,
+            transport: redirect ? `beacon` : ``,
+            hitCallback: function() {
+              if (redirect) {
+                document.location = props.href
+              }
+            }
+          })
+        } else {
+          if (redirect) {
+            document.location = props.href
+          }
+        }
+
+        return false
+      }}
+    />
+  )
+}
+
+OutboundLink.propTypes = {
+  href: PropTypes.string,
+  target: PropTypes.string,
+  onClick: PropTypes.func
+}
+
+// Since DOM elements <a> cannot receive activeClassName
+// and partiallyActive, destructure the prop here and
+// pass it only to GatsbyLink
+const Link = ({ children, to, activeClassName, partiallyActive, ...other }) => {
+  // Tailor the following test to your environment.
+  // This example assumes that any internal link (intended for Gatsby)
+  // will start with exactly one slash, and that anything else is external.
+  const internal = /^\/(?!\/)/.test(to)
+
+  // Use Gatsby Link for internal links, and <a> for others
+  if (internal) {
+    return (
+      <GatsbyLink
+        to={to}
+        activeClassName={activeClassName}
+        partiallyActive={partiallyActive}
+        {...other}
+      >
+        {children}
+      </GatsbyLink>
+    )
+  }
+  return (
+    <OutboundLink href={to} {...other}>
+      {children}
+    </OutboundLink>
+  )
+}
+
+export default Link
